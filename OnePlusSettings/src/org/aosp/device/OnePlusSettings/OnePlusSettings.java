33382adca62ef9f1a5ce78ea5ee549532e18d79c
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2021-06-10 20:13:47 +0530

    kebab: Implement OnePlus Dolby support
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Co-authored-by: darkobas <darkobas@gmail.com>
    Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>
    Co-authored-by: HELLBOY017 <abhaygill017@gmail.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- OnePlusSettings/src/org/aosp/device/OnePlusSettings/OnePlusSettings.java
+++ OnePlusSettings/src/org/aosp/device/OnePlusSettings/OnePlusSettings.java
@@ -41,6 +41,7 @@ import androidx.preference.PreferenceScreen;
 
 import org.aosp.device.OnePlusSettings.ModeSwitch.EdgeTouchSwitch;
 import org.aosp.device.OnePlusSettings.ModeSwitch.DCModeSwitch;
+import org.aosp.device.OnePlusSettings.ModeSwitch.DolbySwitch;
 import org.aosp.device.OnePlusSettings.ModeSwitch.GameModeSwitch;
 import org.aosp.device.OnePlusSettings.ModeSwitch.HBMModeSwitch;
 import org.aosp.device.OnePlusSettings.Preferences.CustomSeekBarPreference;
@@ -78,10 +79,12 @@ public class OnePlusSettings extends PreferenceFragment
     public static final String KEY_EDGE_TOUCH = "edge_touch";
 
     private static final String PREF_DOZE = "advanced_doze_settings";
+    private static final String KEY_ENABLE_DOLBY_ATMOS = "enable_dolby_atmos";
 
     private static final String FILE_LEVEL = "/sys/devices/platform/soc/a8c000.i2c/i2c-3/3-005a/leds/vibrator/level";
     public static final String DEFAULT = "3";
 
+    private static DolbySwitch mDolbySwitch;
     private static ListPreference mFpsInfoColor;
     private static ListPreference mFpsInfoPosition;
     private static ListPreference mNrModeSwitcher;
@@ -94,6 +97,7 @@ public class OnePlusSettings extends PreferenceFragment
     private static SwitchPreference mEdgeTouchSwitch;
     private static SwitchPreference mAutoHBMSwitch;
     private static SwitchPreference mMuteMedia;
+    private static SwitchPreference mEnableDolbyAtmos;
 
     private static CustomSeekBarPreference mFpsInfoTextSizePreference;
     private static VibratorStrengthPreference mVibratorStrengthPreference;
@@ -167,6 +171,11 @@ public class OnePlusSettings extends PreferenceFragment
         mNrModeSwitcher = (ListPreference) findPreference(KEY_NR_MODE_SWITCHER);
         mNrModeSwitcher.setOnPreferenceChangeListener(this);
 
+        mDolbySwitch = new DolbySwitch(getContext());
+        mEnableDolbyAtmos = (SwitchPreference) findPreference(KEY_ENABLE_DOLBY_ATMOS);
+        mEnableDolbyAtmos.setChecked(mDolbySwitch.isCurrentlyEnabled());
+        mEnableDolbyAtmos.setOnPreferenceChangeListener(this);
+
         mGameModeSwitch = (SwitchPreference) findPreference(KEY_GAME_SWITCH);
         mGameModeInfo = (Preference) findPreference(KEY_GAME_INFO);
         if (isGamingModeSupported()) {
@@ -227,6 +236,8 @@ public class OnePlusSettings extends PreferenceFragment
             } else {
                 getContext().stopServiceAsUser(fpsinfo, UserHandle.CURRENT);
             }
+        } else if (preference == mEnableDolbyAtmos) {
+            mDolbySwitch.setEnabled((Boolean) newValue);
         } else if (preference == mFpsInfoPosition) {
             int position = Integer.parseInt(newValue.toString());
             Context mContext = getContext();
--- device.mk
+++ device.mk
@@ -354,6 +354,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_performance_kona.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_kona.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml \
+    $(LOCAL_PATH)/configs/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
     $(LOCAL_PATH)/configs/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_kona.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_kona.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml \
@@ -415,6 +416,13 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     OnePlusSettings
 
+# OnePlusDolby
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_ODM)/etc/dolby/dax-default.xml
+
+PRODUCT_PACKAGES += \
+    OPSoundTunerOverlay
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
@@ -449,6 +457,10 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
 
 # Surface Flinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
