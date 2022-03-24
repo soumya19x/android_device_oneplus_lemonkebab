930fe220d013cba51c0b2a27b08407c29e887db5
dlwlrma123 <alexfinhart@gmail.com>
2022-03-25 01:37:37 +0800

    lemonkebab: enable usb debugging by default
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- device.mk
+++ device.mk
@@ -637,5 +637,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
+# USB Debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1 
+    
 # Inherit from vendor blobs
 $(call inherit-product, vendor/oneplus/lemonkebab/lemonkebab-vendor.mk)
