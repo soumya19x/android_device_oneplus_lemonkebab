a2f629b89b4019350836213380847f1067e893e6
dlwlrma123 <alexfinhart@gmail.com>
2022-04-09 17:15:46 +0800

    lemonkebab: initialize Project Arcana
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- aosp_lemonkebab.mk
+++ aosp_lemonkebab.mk
@@ -21,14 +21,25 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lemonkebab device
 $(call inherit-product, device/oneplus/lemonkebab/device.mk)
 
-# Inherit some common Nameless AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Aosp stuff.
+$(call inherit-product, vendor/aosp/common.mk)
 
-# Boot Animation
+# ArcanaOS
+ARCANA_DEVICE := Oneplus-8T/9R
+ARCANA_MAINTAINER := Soumyo/IU
+ARCANA_OFFICIAL := true
+COPY_APN_SYSTEM := true
+
+# AOSP COMMON
+USE_PIXEL_CHARGING := true
+EXTRA_UDFPS_ANIMATIONS := true
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# Official
-CUSTOM_BUILD_TYPE := Official
+TARGET_BUILD_GRAPHENEOS_CAMERA := false
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+USE_PIXEL_CHARGER_IMAGES := true
+TARGET_SUPPORTS_NOW_PLAYING := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_lemonkebab
@@ -38,3 +49,8 @@ PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := LE2101
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+BUILD_FINGERPRINT := "google/raven/raven:12/SP2A.220305.013.A3/8229987:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="raven-user 12 SP2A.220305.013 8229987 release-keys"
