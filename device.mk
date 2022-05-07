684b0bd77e1cca16bf8df6696158b8aed005d1a6
dlwlrma123 <alexfinhart@gmail.com>
2022-05-08 06:39:25 +0800

    lemonkebab: bump display targets

--- device.mk
+++ device.mk
@@ -307,41 +307,15 @@ PRODUCT_PACKAGES += \
     libvulkan \
     memtrack.kona \
     vendor.display.config@1.5 \
-    vendor.display.config@1.9 \
-    vendor.display.config@1.11 \
-    vendor.display.config@1.14 \
+    vendor.display.config@1.11.vendor \
     vendor.display.config@2.0 \
-    vendor.display.config@1.0.vendor \
-    vendor.display.config@1.1.vendor \
-    vendor.display.config@1.2.vendor \
-    vendor.display.config@1.3.vendor \
-    vendor.display.config@1.4.vendor:64 \
-    vendor.display.config@1.5.vendor:64 \
-    vendor.display.config@1.6.vendor:64 \
-    vendor.display.config@1.7.vendor:64 \
-    vendor.display.config@1.8.vendor:64 \
-    vendor.display.config@1.8.vendor:64 \
-    vendor.display.config@1.9.vendor:64 \
-    vendor.display.config@1.11.vendor:64 \
-    vendor.display.config@1.12.vendor:64 \
-    vendor.display.config@1.13.vendor:64 \
-    vendor.display.config@1.14.vendor:64 \
-    vendor.display.config@2.0.vendor:64 \
-    vendor.qti.hardware.display.allocator@3.0.vendor:64 \
-    vendor.qti.hardware.display.allocator@4.0.vendor:64 \
+    vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator-service \
-    vendor.qti.hardware.display.composer@3.0.vendor:64 \
-    vendor.qti.hardware.display.composer@3.1.vendor:64 \
     vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapper@4.0.vendor \
-    vendor.qti.hardware.display.mapperextensions@1.0.vendor \
-    vendor.qti.hardware.display.mapperextensions@1.1.vendor \
-    vendor.qti.hardware.display.mapperextensions@1.2.vendor \
-    vendor.qti.hardware.display.demura@1.0.vendor \
-    vendor.qti.hardware.display.demura@2.0.vendor
+    vendor.qti.hardware.display.mapper@4.0.vendor
 
 
 # DRM
